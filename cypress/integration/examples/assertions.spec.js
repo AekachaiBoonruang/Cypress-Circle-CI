8964247eb59259ba176210bd96e373d70556627c
aekachai <sniperaek@gmail.com>
2019-06-17 14:46:41 +0700

    test

--- cypress/integration/examples/assertions.spec.js
+++ cypress/integration/examples/assertions.spec.js
@@ -10,7 +10,7 @@ context('Assertions', () => {
       // https://on.cypress.io/should
       cy.get('.assertion-table')
         .find('tbody tr:last')
-        .should('have.class', 'success')
+        .should('have.class', 'successe')
         .find('td')
         .first()
         // checking the text of the <td> element in various ways
